f16c30a48e2aad8246485c5b407eb3f86f72cb18
p0ptartz <ericwm420@gmail.com>
2023-02-17 22:05:03 -0500

    added dept, role, employee prompt questions

--- index.js
+++ index.js
@@ -3,12 +3,94 @@ const mysql = require("mysql2")
 const cTable = require("console.table")
 require("dotenv").config()
 
+// linking to database
 const db = mysql.createConnection(
     {
-      host: '127.0.0.1',
-      user: process.env.DB_USER,
-      password: "",
-      database: process.env.DB_NAME
+        host: '127.0.0.1',
+        user: process.env.DB_USER,
+        password: "",
+        database: process.env.DB_NAME
     }
 );
 
+// main prompt questions
+const main = [
+    {
+        type: "list",
+        name: "mainChoice",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Deparrtments",
+            "Add Department"
+        ]
+    }
+]
+
+// Questions to be asked when asked to add role, dept, or employee -- put these in a function .then take the data and store into table
+const department = [
+    {
+        type: "input",
+        name: "departmentName",
+        message: "What is the name of the department?"
+    }
+]
+
+const role = [
+    {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of the role?",
+    },
+    {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary?",
+    },
+    {
+        type: "list",
+        name: "departmentId",
+        choices: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+]
+
+const employee = [
+    {
+        type: "input",
+        name: "firstName",
+        message: "What is the employees first name?"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the employees last name?"
+    },
+    {
+        type: "input",
+        name: "role",
+        message: "What is the employees role?"
+    },
+    {
+        type: "input",
+        name: "manager",
+        message: "Who is this employee's manager?"
+    }
+]
+
+
+
+
+const init = () => {
+    inquirer.prompt()
+}
+
+init()
